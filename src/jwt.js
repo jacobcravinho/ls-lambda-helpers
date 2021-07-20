c2f1bcaa751d7d6b1a02793d16f4b65289fbdc3a
Santiago Lucca de Guevara <61291091+santiaglde@users.noreply.github.com>
2021-07-20 15:42:23 -0300

    Added JWT Helper
    With two functions:
    decode: Parses a token
    hasExpired: Parses a token and validates if it has expired

---
+++ src/jwt.js
@@ -0,0 +1,26 @@
+const decode = (token) => {
+  const base64Url = token.split('.')[1];
+
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+
+  const buffer = new Buffer.from(base64, 'base64');
+
+  const payloadInit = buffer.toString('ascii');
+
+  const decodedToken = JSON.parse(payloadInit);
+
+  return decodedToken;
+}
+
+const hasExpired = (token) => {
+  const { exp } = decode(token);
+
+  const expirationDate = new Date(exp * 1000);
+
+  return new Date() > expirationDate;
+}
+
+module.exports = {
+  decode,
+  hasExpired
+};
