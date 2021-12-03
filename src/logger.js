a77caf2f267fe0bfcb6bfd8610eea2c1e98935fb
Cristóbal Gajardo Vera <cgajardo@baufest.com>
2021-12-03 14:19:15 -0300

    Added JSDocs to Logger class and methods

--- src/logger.js
+++ src/logger.js
@@ -1,71 +1,107 @@
-/* eslint class-methods-use-this: ["error", { "exceptMethods": ["audit"] }] */
-
+/**
+ * Log messages to the console in a consistent format.
+ */
 class Logger {
+  #isProduction = false;
+  #logLevel = 3;
+  #logLevelMap = {
+    error: 1,
+    info: 2,
+    debug: 3,
+  };
+
+  /**
+   * Create a new Logger instance.
+   */
   constructor() {
-    var effectiveStage = process.env.stage || process.env.STAGE
-    
-    this.isProduction = effectiveStage.toLowerCase().startsWith('prod');
-    this.logLevelMap = { debug: 3, info: 2, error: 1 };
-    this.logLevel = this.setLogLevel();
+    const stage = process.env.stage || process.env.STAGE;
+
+    if (!stage) throw new Error('No stage specified');
+
+    this.#isProduction = stage.toLowerCase().startsWith('prod');
+    this.#logLevel = this.#setLogLevel();
   }
 
-  setLogLevel() {
-    var effectiveLogLevel = process.env.logLevel || process.env.LOG_LEVEL
+  #setLogLevel() {
+    const logLevel = process.env.logLevel || process.env.LOG_LEVEL;
 
-    return this.logLevelMap[effectiveLogLevel];
+    if (!logLevel) throw new Error('No log level specified');
+
+    return this.#logLevelMap[logLevel.toLowerCase()];
   }
 
+  #formatMessage(message) {
+    return `${process.env.stackName.toUpperCase()}: ${JSON.stringify(message)}`;
+  }
+
+  /**
+   * Used to log a message to the console when debug log level is enabled.
+   *
+   * @param  {...any} message Message to log, can be anything that can be stringified.
+   */
   debug(...message) {
-    if (this.logLevel < 3) {
-      return;
-    }
-    console.debug(
-      `${process.env.stackName.toUpperCase()}: ${JSON.stringify(message)}`
-    );
+    if (this.logLevel < 3) return;
+
+    console.debug(this.#formatMessage(message));
   }
 
-  debugNoStringify(label, message) {
-    if (this.logLevel < 3) {
-      return;
-    }
+  /**
+   * Used to log a message to the console without stringifying it.
+   * It doesn't depends on the log level.
+   * Need to be labeled by hand.
+   *
+   * @param {string} label Label to log.
+   * @param {...any} message Message to log, can be anything.
+   */
+  debugNoStringify(label, ...message) {
+    if (this.logLevel < 3) return;
+
     console.debug(label, message);
   }
 
+  /**
+   * Used to log a message to the console when info or debug log level is enabled.
+   *
+   * @param  {...any} message Message to log, can be anything that can be stringified.
+   */
   info(...message) {
-    if (this.logLevel < 2) {
-      return;
-    }
-    console.info(
-      `${process.env.stackName.toUpperCase()}: ${JSON.stringify(message)}`
-    );
+    if (this.logLevel < 2) return;
+
+    console.info(this.#formatMessage(message));
   }
 
+  /**
+   * Used to log a message to the console when any log level is enabled.
+   * If message provided is an Error, it will be logged with the stack trace.
+   *
+   * @param  {...any} message Message to log, can be anything that can be stringified.
+   */
   error(...message) {
-    if (this.logLevel < 1) {
-      return;
-    }
-    if (message instanceof Error) {
-      console.error(message);
-    } else {
-      console.error(
-        `${process.env.stackName.toUpperCase()}: ${JSON.stringify(message)}`
-      );
-    }
+    if (this.logLevel < 1) return;
+
+    if (message instanceof Error) console.error(message);
+    else console.error(this.#formatMessage(message));
   }
 
+  /**
+   * Used to log a message to the console without depends on the log level.
+   *
+   * @param  {...any} message Message to log, can be anything that can be stringified.
+   */
   audit(...message) {
-    console.log(
-      `${process.env.stackName.toUpperCase()}: ${JSON.stringify(message)}`
-    );
+    console.log(this.#formatMessage(message));
   }
 
+  /**
+   * Used to log a message to the console when any log level is enabled.
+   * Doesn't works if production environment is enabled.
+   *
+   * @param  {...any} message Message to log, can be anything that can be stringified.
+   */
   log(...message) {
-    if (this.isProduction) {
-      return;
-    }
-    console.log(
-      `${process.env.stackName.toUpperCase()}: ${JSON.stringify(message)}`
-    );
+    if (this.isProduction) return;
+
+    console.log(this.#formatMessage(message));
   }
 }
 
